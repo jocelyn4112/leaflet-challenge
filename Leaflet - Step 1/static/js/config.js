fa16d4a65e2286c250becc4c161ff005e0d4bc9d
jocelyn4112 <68340393+jocelyn4112@users.noreply.github.com>
2021-01-26 18:53:39 -0500

    Add map

--- Leaflet - Step 1/static/js/config.js
+++ Leaflet - Step 1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "prj_live_sk_e2c8ae2f2358959780178a5dde6369a9a95f726a"
+const API_KEY = "pk.eyJ1Ijoiam9jZWx5bjQxMTIiLCJhIjoiY2tpczF1NXN4MDh0ZjJyb2F3d2djdTI2diJ9.xCszkZTryorXjMXK8b4jpw"
